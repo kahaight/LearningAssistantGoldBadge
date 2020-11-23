34327fab5a0a4240e346b965899965f09a30ec9b
Konrad Haight <konradhaight@gmail.com>
2020-11-23 18:56:45 -0500

    inheritance

---
+++ CSharpFundamentals/07_Inheritance/InheritanceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _07_Inheritance
+{
+    [TestClass]
+    public class InheritanceTests
+    {
+        [TestMethod]
+        public void PersonExamples()
+        {
+            var customer = new Customer(true, "Konrad", "Haight", "555-555-5555", "konradhaight@gmail.com" );
+
+            var employee = new Employee();
+            employee.FirstName = "Luke";
+            employee.HireDate = DateTime.Today;
+
+            var hourlyEmployee = new HourlyEmployee();
+            hourlyEmployee.FirstName = "Kim";
+
+            List<Person> people = new List<Person>();
+
+            people.Add(customer);
+            people.Add(hourlyEmployee);
+            people.Add(employee);
+
+        }
+    }
+}
---
+++ CSharpFundamentals/07_Inheritance/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("07_Inheritance")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("07_Inheritance")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("efa405ac-3de3-4696-8856-24741bb2fb92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
